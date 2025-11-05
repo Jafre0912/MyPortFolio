a0c8d9aca8981fd22671bbebc3b98bf4af1b83ce
jafre alam <jafrea835@gmail.com>
2025-11-05 08:02:40 +0530

    Updated portfolio design and content

--- src/routes/components/WorkCardData.js
+++ src/routes/components/WorkCardData.js
@@ -10,7 +10,7 @@ const ProjectCardData = [
         imgsrc: pro1,
         title: "Bakery Hub Website Design",
         Text: "Thriving bakery community and top-ranked platform, The Bakery Hub inspires a delightful, creative, and rewarding sense of Wow! I really can bake and sell! for passionate home bakers and bakery business owners. Offering the best-selling baking guides and marketing tips, we help turn your love for baking into a thriving business. 🍪🎂",
-        view: "https://deft-sunshine-de7405.netlify.app/"
+        view: "https://ecommercefrontendaj.netlify.app/"
     },
     {
         imgsrc: pro2,
